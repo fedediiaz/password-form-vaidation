ada935a583aaf468286d9f6ea5a23dea1a90ca90
Your name here <Your e-mail here>
2020-07-05 12:12:03 +0200

    added Toggler, factored some code

--- pwValidator.js
+++ pwValidator.js
@@ -2,12 +2,14 @@
     // GLobals
     const form = document.querySelector("#registration-form");
     const passwordFields = document.querySelectorAll("input[type='password']");
+    const passwordToggler = document.querySelectorAll(".toggleView");
 
     function checkPasswordValidations(word) {
         /* Input  => String
            Output => Object 
         */
 
+        // Validations to be made over the string. 
         const pattern = {
             uppercase: /[A-Z]/,
             lowercase: /[a-z]/,
@@ -22,6 +24,8 @@
             },
         };
 
+        
+        // Must be a better way to do the bellow code
         return {
             uppercase: pattern.uppercase.test(word),
             lowercase: pattern.lowercase.test(word),
@@ -32,7 +36,7 @@
     }
 
     function isValidPassword(objPassword) {
-        /* Input  => Object
+        /* Input  => Object from checkPasswordValidations
            Output => bool
         */
         const validationsArray = Object.values(objPassword);
@@ -42,14 +46,7 @@
         });
     }
 
-    function isWordMatch(string1, string2) {
-        /* Input  => string, string
-           Output => bool 
-        */
-        return string1 === string2;
-    }
-
-    function validateHTML(obj) {
+    function UpdatePassValidationsFields(obj) {
         /* Input  => Object from checkPasswordValidations
            Output => none;
            Desc   => Modifies HTML
@@ -66,64 +63,75 @@
         }
     }
 
-    function toggleBetweenClass(target, removeClass, addClass) {
-        target.remove(removeClass);
-        target.add(addClass);
+    function isWordMatch(string1, string2) {
+        /* Input  => string, string
+           Output => bool 
+        */
+        return string1 === string2;
+    }
+
+    function toggleClasses(target, removeClass, addClass) {
+        const targetClass = target.classList;
+
+        targetClass.add(addClass);
+        targetClass.remove(removeClass);
     }
 
     form.addEventListener("keyup", (e) => {
         const password = form.password.value;
         const passwordConfirm = form.confirmPassword.value;
         const submitButton = document.querySelector("button[type='submit']");
-        const targetClass = e.target.classList;
+        const target = e.target
 
         // Validate patterns for a valid password
         const passwordChecker = checkPasswordValidations(password);
-        // Check if every pattern match
+        // Check if every pattern match, only then is a valid password.
         const isValidPW = isValidPassword(passwordChecker);
-        // Check if both passwords match
+        // Check if both passwords and ConfirmPassword match
         const passwordsMatch = isWordMatch(passwordConfirm, password);
 
-        validateHTML(passwordChecker);
+        // Updates HTML with corresponding errors
+        UpdatePassValidationsFields(passwordChecker);
 
-        // Update submit button ONLY when conditions met
+        // Enable submit button ONLY when conditions met
         if (isValidPW && passwordsMatch && submitButton.disabled) {
             submitButton.disabled = false;
         } else if (!submitButton.disabled && !passwordsMatch) {
             submitButton.disabled = true;
         }
 
-        switch (e.target.id) {
+
+        // Toggle Classes
+        switch (target.id) {
             case "password":
                 if (password.length === 0) {
-                    e.target.className = `${targetClass[0]} ${targetClass[1]}`;
+                    target.className = `${target.classList[0]}`;
                 } else if (
-                    isValidPassword(checkPasswordValidations(password))
+                    isValidPassword(
+                        checkPasswordValidations(password)
+                        )
                 ) {
-                    targetClass.add("is-success");
-                    targetClass.remove("is-error");
+                    toggleClasses(target, "is-error", "is-success");
                 } else {
-                    targetClass.add("is-error");
-                    targetClass.remove("is-success");
+                    toggleClasses(target, "is-success", "is-error");
                 }
                 break;
             case "confirmPassword":
                 if (passwordConfirm.length === 0) {
-                    e.target.className = `${targetClass[0]} ${targetClass[1]}`;
+                    target.className = `${target.classList[0]}`;
                 } else if (
                     isValidPassword(
                         checkPasswordValidations(passwordConfirm)
                     ) &&
                     passwordsMatch
                 ) {
-                    targetClass.add("is-success");
-                    targetClass.remove("is-error");
+                    toggleClasses(target, "is-error", "is-success")
                 } else {
-                    targetClass.add("is-error");
-                    targetClass.remove("is-success");
+                    toggleClasses(target, "is-success", "is-error");
                 }
                 break;
         }
+
     });
 
     passwordFields.forEach((field) => {
@@ -136,5 +144,31 @@
         field.addEventListener("blur", () => {
             errors.style.visibility = "hidden";
         });
+
     });
-})();
+
+    passwordToggler.forEach(elem => {
+        
+        elem.addEventListener("click", e => {
+
+        const target = e.target;
+        const curInputField = target.parentElement.previousElementSibling;
+        const hasHiddenClass = target.classList.contains("hidden");
+        const targetSibling = target.previousElementSibling || target.nextElementSibling;
+
+        if (!hasHiddenClass) {
+            targetSibling.classList.toggle("hidden");
+            target.classList.toggle("hidden");
+
+            switch(curInputField.type) {
+                case "text":
+                    curInputField.type = "password";
+                    break;
+                case "password":
+                    curInputField.type = "text";
+                    break;
+            }
+        }
+        });
+    })
+}());
